2818ac69cf196d6514fc036bc9cede94d8e88c00
eaterstrawberry <rimozkovaya@gmail.com>
2023-07-19 21:15:03 +0300

    services

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:30,
+		nav:true,
+		dots: false,
+		startPosition: 1,
+		navText: ['<img src="img/arrow-left.svg">', '<img src="img/arrow-right.svg">'],
+		responsive:{
+			 0:{
+				  items:1
+			 },
+			 600:{
+				  items:1
+			 },
+			 1000:{
+				  items:1
+			 }
+		}
+  });
+});
